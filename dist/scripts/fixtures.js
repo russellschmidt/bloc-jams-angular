d7e1b2ff44f169db00ac171ac57a85be387309af
Russell Schmidt <russell.schmidt@me.com>
2016-03-31 13:23:00 -0700

    add two controllers collection and landing

---
+++ dist/scripts/fixtures.js
@@ -0,0 +1,51 @@
+/* fixtures.js */
+// Example album 1
+var albumPicasso = {
+  title: 'The Colors',
+  artist: 'Pablo Picasso',
+  label: 'Cubism',
+  year: '1881',
+  albumArtUrl: 'assets/images/album_covers/01.png',
+  songs: [
+    { title: 'Blue', duration: 161.71, audioUrl: '/assets/music/blue' },
+    { title: 'Green', duration: 103.96, audioUrl: '/assets/music/green'},
+    { title: 'Red', duration: 268.45, audioUrl: '/assets/music/red'},
+    { title: 'Pink', duration: 153.14, audioUrl: '/assets/music/pink'},
+    { title: 'Magenta', duration: 374.22, audioUrl: '/assets/music/magenta'}
+  ]
+};
+
+// Example album 2
+var albumMarconi = {
+  title: 'The Telephone',
+  artist: 'Guglielmo Marconi',
+  label: 'EM',
+  year: '1909',
+  albumArtUrl: 'assets/images/album_covers/20.png',
+  songs: [
+    { title: 'Hello, Operator?', duration: '1:01'},
+    { title: 'Ring, ring, ring', duration: '5:01'},
+    { title: 'Fits in your pocket', duration: '3:21'},
+    { title: 'Can you hear me now?', duration: '3:14'},
+    { title: 'Wrong phone number', duration: '2:15'}
+  ]
+};
+
+// Example album 3
+var albumXmas = {
+  title: 'The Most Wonderful Time of the Year',
+  artist: 'Scott Weiland',
+  label: 'Sonyversal Brothers',
+  year: '2005',
+  albumArtUrl: 'assets/images/album_covers/22.png',
+  songs: [
+    { title: 'Black Tar Holiday', duration: '2:02'},
+    { title: 'Runny Nosed Reindeer', duration: '3:31'},
+    { title: 'Blacked Out on Christmas Eve', duration: '2:11'},
+    { title: 'Honey I Sold All The Presents (Again)', duration: '6:54'},
+    { title: 'Where the Dog Smells', duration: '4:04'},
+    { title: 'Rocks in my Stocking', duration: '1:59'},
+    { title: 'Myrrh', duration: '1:59'},
+    { title: 'What day is it? (Christmas Morning)', duration: '12:33'}
+  ]
+};
