abd354c7fc219c96579803f8af10cec7e4c7fbe1
Russell Schmidt <russell.schmidt@me.com>
2016-03-31 15:55:20 -0700

    fix durations on album.html via AlbumCtrl

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -6,6 +6,8 @@
 
     for (var i = 0; i < this.albumData.songs.length; i++) {
       this.albumSongs.push(angular.copy(albumPicasso.songs[i]));
+      this.albumSongs[i].duration = Math.floor(this.albumSongs[i].duration/60) + ":" +
+        Math.floor(this.albumSongs[i].duration%60);
     }
   }
 
