5a974591c7a9fead5b12e9649c2998d60c4ae939
Russell Schmidt <russell.schmidt@me.com>
2016-04-26 11:58:20 -0700

    complete checkpoint

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -14,7 +14,9 @@
       templateUrl: '/templates/directives/seek_bar.html',
       replace: true,
       restrict: 'E',
-      scope: { },
+      scope: { 
+        onChange: '&'
+      },
       link: function(scope, element, attributes) {
         
         // directive logic to return
@@ -22,6 +24,14 @@
         scope.max = 100;
         
         var seekBar = $(element);
+        
+        attributes.$observe('value', function(newValue) {
+          scope.value = newValue;
+        });
+        
+        attributes.$observe('max', function(newValue) {
+          scope.max = newValue;
+        })
         
         var percentString = function() {
           var value = scope.value;
@@ -42,6 +52,7 @@
         scope.onClickSeekBar = function(event) {
           var percent = calculatePercent(seekBar, event);
           scope.value = percent * scope.max;
+          notifyOnChange(scope.value);
         };
         
         scope.trackThumb = function() {
@@ -49,6 +60,7 @@
             var percent = calculatePercent(seekBar, event);
             scope.$apply(function() {
               scope.value = percent * scope.max;
+              notifyOnChange(scope.value);
             });
           });
           
@@ -56,6 +68,12 @@
             $document.unbind('mousemove.thumb');
             $document.unbind('mouseup.thumb');
           });
+        };
+        
+        var notifyOnChange = function(newValue) {
+          if (typeof scope.onChange === 'function') {
+            scope.onChange({value: parseFloat(newValue) });
+          }
         };
         
       }
